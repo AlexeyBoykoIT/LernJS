aa9a322178117f0058d93be4b58462fe7825829a
AlexeyBoykoIT <101599356+AlexeyBoykoIT@users.noreply.github.com>
2022-03-15 10:48:06 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+//////// CODWARS //////////////
 
+function disemvowel(str) {
+    let newStr = "",
+        i;
+
+    for (i = 0; i < str.length; i++) {
+        if ("aeiou".includes(str[i].toLowerCase())) continue;
+        newStr += str[i];
+    }
+    return newStr;
+}
